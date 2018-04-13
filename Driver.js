b728f97010867f31ae7b669624763946cb181fe3
rubic <adamtheball@gmail.com>
2018-04-13 14:05:22 -0700

    introducing polyomino solving logic
    decided on making a custom algortihm, wrote pseudocode in preparation

--- Driver.js
+++ Driver.js
@@ -9,30 +9,99 @@ Driver();
 function Driver() {
   // in case we need to reference Driver
   let that = this;
+  // create a Solver object that performs solving
+  let Solver = new ps.Solver();
+
+  // this is the side length of the constructor in pixels
+  let constructorSize;
+  // this is the side length of the constructor in grid squares
+  // this value can be changed by the user
+  let constructorSquareLength;
+
+
+  $(document).ready( function() {
+    init();
+  });
   /*
     This function initiatlizes the Driver when the page loads.
   */
   this.init = function() {
-    console.log("now listening to #polyomino-field-solve");
-
-    $("#polyomino-field-solve").ready( function() {
-      console.log("polyomino-field-solve exists!");
-
-      $(this).on("click", function() {
-         $(this).removeClass("mdl-button--accent").addClass("mdl-button--disabled");
-         $("#polyomino-field-stop").removeClass("mdl-button--disabled").addClass("mdl-button--accent");
-      });
-    });
-
+    // initalize all variables
+    this.constructorSize = 500;
+    this.constructorSquareLength = 4;
+    // initalize the functions of every button
+    this.initButtons();
     // load the grid that shows the Constructor
-    //this.displayConstructorGrid();
+    this.displayConstructorGrid();
   }
+
+  // set a button to disabled, accent, or primary styles
+  this.buttonToDisabled = function( $button ) { $button.removeClass("mdl-button--accent mdl-button--primary").addClass("mdl-button--disabled"); }
+  this.buttonToAccent = function( $button ) { $button.removeClass("mdl-button--disabled mdl-button--primary").addClass("mdl-button--accent"); }
+  // this forcefully hides the drawer
+  this.hideDrawer = function() { $(".mdl-layout__drawer").removeClass("is-visible").attr("aria-hidden", "true"); $(".mdl-layout__obfuscator").removeClass("is-visible"); }
 
   this.displayConstructorGrid = function() {
-    let constructGrid = $( "#polyomino-constructor-svg" )
-    console.log(constructGrid);
+    let $constructorGrid = $("#polyomino-constructor-svg")
+                            .attr("xmlns", "http://www.w3.org/TR/SVG11/")
+                            .attr("width", "500")
+                            .attr("height", "500");
+    let gridSquareLength = 500 / this.constructorSquareLength + "px";
+    let gridSquare = $("<rect></rect>")
+                      .attr("fill", "#ff0000")
+                      .attr("width", gridSquareLength)
+                      .attr("height", gridSquareLength);
+    $constructorGrid.append( gridSquare );
   }
 
-  // after all is said and done, initiatlize Driver
-  this.init();
+  this.initButtons = function() {
+    // grab references to every button that needs to be listened to
+    let $constructorClear = $("#polyomino-constructor-clear");
+    let $constructorSave = $("#polyomino-constructor-save");
+    let $fieldSolve = $("#polyomino-field-solve");
+    let $fieldStop = $("#polyomino-field-stop");
+    let $fieldClear = $("#polyomino-field-clear");
+    let $drawerSave = $("#drawer-local-save");
+    let $drawerLoad = $("#drawer-local-load");
+    let $drawerAbout = $("#drawer-about");
+    let $drawerHelp = $("#drawer-help");
+
+    // when the Clear button on the Constructor screen is clicked
+    $constructorClear.on("click", ()=>{
+      console.log("The Constructor was cleared.");
+    });
+
+    $constructorSave.on("click", ()=>{
+      console.log("The polyomino in the Constructor was saved.");
+    });
+
+    // when the Solve button is pressed
+    $fieldSolve.on("click", ()=>{
+      that.buttonToDisabled($fieldSolve);
+      that.buttonToAccent($fieldStop);
+      Solver.solve();
+    });
+
+    // when the Stop button is pressed
+    $fieldStop.on("click", ()=>{
+      that.buttonToDisabled($fieldStop);
+      that.buttonToAccent($fieldSolve);
+      console.log("SOLVING WAS STOPPED.");
+    });
+
+    // when the Clear button on the Field screen is clicked
+    $fieldClear.on("click", ()=> {
+      console.log("The field was cleared.");
+    });
+
+    $drawerSave.on("click", ()=> {
+      console.log("Local save committed.");
+      that.hideDrawer();
+    });
+
+    $drawerLoad.on("click", ()=> {
+      console.log("Local load committed.");
+      that.hideDrawer();
+    })
+  }
 }
--- Solver.js
+++ Solver.js
@@ -6,12 +6,26 @@ console.log("Creating the Solver.");
 ps.Solver = function() {
 
   this.init = function() {
-
+    console.log("Solver initiatlized.");
   }
 
   this.solve = function() {
     console.log("CURRENTLY SOLVING...");
+
+    // let polyominoes = array of Polyominoes the user wants to fill with
+    // let field = array of Vec2 the user wants to fill
+    // let polyexpands = []; array of Polyexpand objects
+    // let rules = [true, false]; array of rules
+
+    // FIRST, ESTABLISH RULES FOR POLYEXPANDS
+
+    // PLUG POLYOMINOES INTO POLYEXPANDS
+    // for( let i = 0, i < polyominoes.length; i++ )
+      // polyexpands[i] = new ps.Polyexpand( polyominoes[i], field, rules );
+
+    // SO THE POLYOMINOES EXPAND INTO ALL OF THEIR OTHER FORMS BASED ON RULES AND THE FIELD
+    // NOW, WE WILL USE POLYFIT OBJECTS TO BEGIN 
   }
 
-  init();
+  this.init();
 }
