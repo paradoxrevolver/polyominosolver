abb2b0670fd8468b6fcfc0fc469f3c7b2fd99dc3
Steven V <7stevenvuong@gmail.com>
2018-04-30 15:46:30 -0700

    skeleton of code

---
+++ Save.js
@@ -0,0 +1,21 @@
+/*
+ * using Node.js perhaps?
+ */
+
+ps.SaveObject = function(aPalette, aBank) {
+    let that = this;
+
+    var obj = {
+        palette: [],
+        bank: []
+    };
+
+    that.init = function(aPalette, aBank) {
+        /*
+         * push polyominoes to palette and bank
+         * 
+         */
+
+    }
+
+}
