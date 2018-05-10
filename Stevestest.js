35e4269ae957848536e88897a92824349711e889
Steven V <7stevenvuong@gmail.com>
2018-05-10 15:21:42 -0700

    Delete Stevestest.js

--- Stevestest.js
+++
@@ -1,6 +0,0 @@
-var mylist = [];
-mylist.push({source: "mysource", item: "myitem"});
-mylist.push({source: "mysource11", item: "myitem11"});
-mylist.push({source: "mysource", item: ["y","z"]});
-
-console.log(mylist);
